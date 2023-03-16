067cc8fc45b934ba020d34db16b96c2334a42f33
Peter Hudec <peterhudec@peterhudec.com>
2023-03-16 15:41:34 +0000

    Simplified tests. They now make assertions about invariants

--- src/utils/percentageDistribution.test.js
+++ src/utils/percentageDistribution.test.js
@@ -1,4 +1,5 @@
 import { toBeDeepCloseTo,toMatchCloseTo } from 'jest-matcher-deep-close-to'
+import _ from 'lodash'
 
 import { percentageDistribution } from './percentageDistribution'
 
@@ -8,87 +9,60 @@ expect.extend({toBeDeepCloseTo, toMatchCloseTo})
 const fractionStringToNumber = eval
 
 describe('percentageDistribution', () => {
-  describe('uniform distribution', () => {
-    ;[
-      {
-        args: [
-          ['1/2', '1/2'],
-          0,
-          '1/4',
-        ],
-        expected: ['1/4', '3/4']
-      },
-      {
-        args: [
-          ['1/2', '1/2'],
-          0,
-          '1/7',
-        ],
-        expected: ['1/7', '6/7']
-      },
-      {
-        args: [
-          ['1/3', '1/3', '1/3'],
-          0,
-          '1/2',
-        ],
-        expected: ['1/2', '1/2/2', '1/2/2']
-      },
-      {
-        args: [
-          ['1/4', '1/4', '1/4', '1/4'],
-          0,
-          '1/2',
-        ],
-        expected: ['1/2', '1/2/3', '1/2/3', '1/2/3'],
-      },
-      {
-        args: [
-          ['1/4', '1/4', '1/4', '1/4'],
-          2,
-          '1/2',
-        ],
-        expected: ['1/2/3', '1/2/3', '1/2', '1/2/3'],
-      },
-    ].forEach(({args: [values, index, newValue], expected}) => {
-      test(`percentageDistribution([${values.join(', ')}], ${index}, ${newValue})`, () => {
+  ;[
+    [
+      ['1/2', '1/2'],
+      0,
+      '1/4',
+    ],
+    [
+      ['1/3', '1/3', '1/3'],
+      0,
+      '1/2',
+    ],
+    [
+      ['1/4', '1/4', '1/4', '1/4'],
+      0,
+      '1/2',
+    ],
+    [
+      ['1/2', '1/4', '1/8', '1/8'],
+      2,
+      '1/3',
+    ],
+  ].forEach(([values, index, newValue]) =>  {
+    test(
+      `percentageDistribution([${values.join(', ')}], ${index}, ${newValue})`,
+      () => {
+        const numericValues = values.map(fractionStringToNumber)
+        const numericNewValue = fractionStringToNumber(newValue)
+
         const result = percentageDistribution(
-          values.map(fractionStringToNumber),
+          numericValues,
           index,
-          fractionStringToNumber(newValue),
+          numericNewValue,
         )
 
-        expect(result).toBeDeepCloseTo(expected.map(fractionStringToNumber), 10)
-      })
-    })
-  })
+        expect(result[index]).toBe(numericNewValue)
+        expect(_.sum(result)).toBeCloseTo(1, 10)
 
-  describe('non uniform distribution', () => {
-    // TODO: Can this test be generalized?
-    test('foo', () => {
-      const values = [
-        '1/2',
-        '1/4',
-        '1/8',
-        '1/8',
-      ].map(fractionStringToNumber)
-      const result = percentageDistribution(
-        values,
-        2,
-        1/3,
-      )
-      expect(result[2]).toBe(1/3)
-      // Result should sum to 1
-      expect(result.reduce((a, x) => a + x, 0)).toBeCloseTo(1, 10)
-
-      // The other values should be in same proportion to each other
-      expect(values[0] / result[0]).toBe(values[1] / result[1])
-      expect(values[1] / result[1]).toBe(values[3] / result[3])
-      expect(values[3] / result[3]).toBe(values[0] / result[0])
-
-      expect(values[0] / values[1]).toBe(result[0] / result[1])
-      expect(values[1] / values[3]).toBe(result[1] / result[3])
-      expect(values[3] / values[0]).toBe(result[3] / result[0])
-    })
+        // All ratios of old to new values should be the same,
+        // except for the values at index
+        expect(
+          // Compute ratios of previous to new values
+          _(numericValues)
+          .zipWith(result, (a, b) => a / b)
+          // The invariant doesn't apply for the value at index, so we remove it
+          .omit(index)
+          // omit resolves with an object, so we need to convert it back to array
+          .values()
+          // All the ratios should be the same, so after deduplicating...
+          .uniq()
+          .value()
+          // ...there should be only a single item in the array
+          .length
+        ).toBe(1)
+      }
+    )
   })
 })
