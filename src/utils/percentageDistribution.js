7f1ce4e6d14ed26a5f588566c07098128489b6c3
Peter Hudec <peterhudec@peterhudec.com>
2023-03-16 16:35:34 +0000

    Added test cases for invalid input

--- src/utils/percentageDistribution.js
+++ src/utils/percentageDistribution.js
@@ -1,4 +1,19 @@
+import _ from 'lodash'
+
 export const percentageDistribution = (values, idx, newValue) => {
+  if (_.sum(values) !== 1) {
+    throw Error('Values must have a sum of 1')
+  }
+  if (idx > values.length || idx < 0) {
+    throw Error('Index out of bounds')
+  }
+  if (!_.isInteger(idx)) {
+    throw Error('Index must be an integer')
+  }
+  if (newValue < 0 || newValue > 1) {
+    throw Error('New value must be between 0 and 1')
+  }
+
   // Split the array into the current value and the rest
   const rest = [...values]
   const [currentValue] = rest.splice(idx, 1)
--- src/utils/percentageDistribution.test.js
+++ src/utils/percentageDistribution.test.js
@@ -9,60 +9,126 @@ expect.extend({toBeDeepCloseTo, toMatchCloseTo})
 const fractionStringToNumber = eval
 
 describe('percentageDistribution', () => {
-  ;[
-    [
-      ['1/2', '1/2'],
-      0,
-      '1/4',
-    ],
-    [
-      ['1/3', '1/3', '1/3'],
-      0,
-      '1/2',
-    ],
-    [
-      ['1/4', '1/4', '1/4', '1/4'],
-      0,
-      '1/2',
-    ],
-    [
-      ['1/2', '1/4', '1/8', '1/8'],
-      2,
-      '1/3',
-    ],
-  ].forEach(([values, index, newValue]) =>  {
-    test(
-      `percentageDistribution([${values.join(', ')}], ${index}, ${newValue})`,
-      () => {
-        const numericValues = values.map(fractionStringToNumber)
-        const numericNewValue = fractionStringToNumber(newValue)
+  describe('invalid input', () => {
+    ;[
+      {
+        args: [
+          [1/2, 1/2, 1/2],
+          0,
+          1/4,
+        ],
+        expectedErrorMessage: 'Values must have a sum of 1',
+      },
+      {
+        args: [
+          [1/4, 1/4, 1/4],
+          0,
+          1/4,
+        ],
+        expectedErrorMessage: 'Values must have a sum of 1',
+      },
+      {
+        args: [
+          [1/4, 1/4, 1/4, 1/4],
+          5,
+          1/4,
+        ],
+        expectedErrorMessage: 'Index out of bounds',
+      },
+      {
+        args: [
+          [1/4, 1/4, 1/4, 1/4],
+          -1,
+          1/4,
+        ],
+        expectedErrorMessage: 'Index out of bounds',
+      },
+      {
+        args: [
+          [1/4, 1/4, 1/4, 1/4],
+          1.5,
+          1/4,
+        ],
+        expectedErrorMessage: 'Index must be an integer',
+      },
+      {
+        args: [
+          [1/4, 1/4, 1/4, 1/4],
+          0,
+          1.0001,
+        ],
+        expectedErrorMessage: 'New value must be between 0 and 1',
+      },
+    ].forEach(({args, expectedErrorMessage}) => {
+      test(`should throw: ${expectedErrorMessage}`, () => {
+        expect(() => percentageDistribution(...args))
+          .toThrowError(expectedErrorMessage)
+      })
+    })
+  })
 
-        const result = percentageDistribution(
-          numericValues,
-          index,
-          numericNewValue,
-        )
+  describe('valid input', () => {
+    ;[
+      [
+        ['1/2', '1/2'],
+        0,
+        '1/4',
+      ],
+      [
+        ['1/3', '1/3', '1/3'],
+        0,
+        '1/2',
+      ],
+      [
+        ['1/4', '1/4', '1/4', '1/4'],
+        0,
+        '1/2',
+      ],
+      [
+        ['1/2', '1/4', '1/8', '1/8'],
+        2,
+        '1/3',
+      ],
+      [
+        ['2/41', '3/41', '5/41', '7/41', '11/41', '13/41'],
+        0,
+        '37/53',
+      ],
+    ].forEach(([values, index, newValue]) =>  {
+      test(
+        `percentageDistribution([${values.join(', ')}], ${index}, ${newValue})`,
+        () => {
+          const numericNewValue = fractionStringToNumber(newValue)
+          const numericValues = values.map(fractionStringToNumber)
 
-        expect(result[index]).toBe(numericNewValue)
-        expect(_.sum(result)).toBeCloseTo(1, 10)
+          const result = percentageDistribution(
+            numericValues,
+            index,
+            numericNewValue,
+          )
 
-        // All ratios of old to new values should be the same,
-        // except for the values at index
-        expect(
-          // Compute ratios of previous to new values
-          _(numericValues)
-          .zipWith(result, (a, b) => a / b)
-          // The invariant doesn't apply for the value at index, so we remove it
-          .omit(index)
-          // omit resolves with an object, so we need to convert it back to array
-          .values()
-          // All the ratios should be the same, so after deduplicating...
-          .uniq()
-          .value()
-          // ...there should be only a single item in the array
-          .length
-        ).toBe(1)
-      }
-    )
+          expect(result[index]).toBe(numericNewValue)
+          expect(_.sum(result)).toBeCloseTo(1, 10)
+
+          // Compute the previous/new value ratios excluding the item at index
+          const uniqueRatios = _(numericValues)
+            .zipWith(result, (a, b) => a / b)
+            // The invariant doesn't apply for the value at index, so we remove it
+            .omit(index)
+            // omit resolves with an object, so we need to convert it back to array
+            .values()
+            // There could be slight differences for the same ratios, due to
+            // precision issues, so we round the values to a pretty high precision
+            .map(x => _.round(x, 10))
+            // All the ratios should be the same, so after deduplicating...
+            .uniq()
+            .value()
+
+          // All ratios of old to new values should be the same,
+          // except for the values at index
+          expect(uniqueRatios).toHaveLength(1)
+        }
+      )
+    })
   })
 })
